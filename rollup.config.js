3eb14b771665fb3544e23cc77a5abf515d22ac13
Toan <me@toan.io>
2020-12-22 10:43:19 +0700

    re-add public files

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import svelte from 'rollup-plugin-svelte';
-import commonjs from '@rollup/plugin-commonjs'
+import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
